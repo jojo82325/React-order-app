b726d2ceda88a925cb200930fe8acc71c38658ad
Alicia Yu <aliciayu.nz@gmail.com>
2018-04-17 15:52:31 +1200

    Route not working

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,11 @@ class Header extends React.Component {
                     <span className="ofThe">
                         <span className="of">Of</span>
                         <span className="the">The</span>
-
                     </span>
                     Day
                     </h1>
-                <h3>
-                    <span>Fresh Daily</span>
+                <h3 className="tagline">
+                    <span>{this.props.tagline}</span>
                 </h3>
 
             </header>
