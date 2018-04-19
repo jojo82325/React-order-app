de0c063a7a276e5faefd3c237ecbc16efa8e8f29
Alicia Yu <aliciayu.nz@gmail.com>
2018-04-19 23:47:16 +1200

    Order state finished

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { formatPrice } from "../helpers";
 class Fish extends React.Component {
-  handleClick = () => {
-    this.props.addToOrder(this.props.index);
-  };
-
   render() {
     const { image, name, price, desc, status } = this.props.details;
     const isAvailable = status === "available";
