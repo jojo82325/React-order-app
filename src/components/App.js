fd3d703ec1c6784c676312c84cb0b42f6adad74f
Alicia Yu <aliciayu.nz@gmail.com>
2018-04-19 18:52:55 +1200

    Windows run success

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
-import SampleFishes from "../sample-fishes";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
@@ -11,31 +12,52 @@ class App extends React.Component {
   };
 
   addFish = fish => {
-      // 1. Take a copy of the exsiting state
-      const fishes = {...this.state.fishes}
-      // 2. Add our new fish to that fishes variable
-      fishes[`fish${Date.now()}`] = fish;
-      // 3. set the new fishes object to state
-      this.setState({
-        //   fishes: fishes
-        fishes
-      });
+    // 1. Take a copy of the exsiting state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object to state
+    this.setState({
+      //   fishes: fishes
+      fishes
+    });
   };
 
-  loadSampleFishes = ()=>{
-      this.setState({
-          fishes: SampleFishes,
-      });
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
   };
 
+  addToOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. call setState to update
+    this.setState({ order });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
